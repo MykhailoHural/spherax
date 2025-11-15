4e299a8d4c380df3fc5320a10d7d544673406763
Mykhailo Hural <32079055+MykhailoHural@users.noreply.github.com>
2025-11-15 15:42:31 +0200

    feat: Implement login smoke test with POM and fixtures (#2)
    * Initial project : Playwright, pnpm, TS, eslint, .env
    
    * feat: Implement login smoke test with POM and fixtures
    
    ---------
    
    Co-authored-by: Mykhailo Hural <mykhailo_hural@gmail.com>

---
+++ src/tests/login.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '../fixtures/page.fixture';
+
+test.describe('Login Page Smoke Test', () => {
+
+  test('should open the page, verify UI and check validation', async ({ loginPage, page }) => {
+
+    await test.step('1: Navigate to the Login Page', async () => {
+      await loginPage.navigateToLogin();
+    });
+
+    await test.step('2: Verify that key UI elements are visible', async () => {
+      await expect(loginPage.logo).toBeVisible();
+      await expect(loginPage.loginButton).toBeVisible();
+    });
+
+    await test.step('3: Click login button and verify validation message', async () => {
+      await loginPage.loginButton.click();
+      await expect(page).toHaveURL(/amazoncognito\.com/, { timeout: 10000 });
+    });
+
+  });
+});
